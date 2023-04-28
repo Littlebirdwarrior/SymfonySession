0eab4c94099282d37dbf1a80c4357d422f883552
Littlebridwarrior <camille.dorland@gmail.com>
2023-04-28 16:57:08 +0200

    findBy interns ok

--- src/Controller/InternController.php
+++ src/Controller/InternController.php
@@ -2,17 +2,21 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Intern;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class InternController extends AbstractController
 {
     #[Route('/intern', name: 'app_intern')]
-    public function index(): Response
+    public function index( ManagerRegistry $doctrine ): Response
     {
+        $interns = $doctrine->getRepository( Intern::class)->findBy([], ["name" => "ASC"]);
+        
         return $this->render('intern/index.html.twig', [
-            'controller_name' => 'InternController',
+            'interns' => $interns,
         ]);
     }
 }
