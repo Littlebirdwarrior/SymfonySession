13745282039a4556f8b910c26077de44419dc13d
Littlebridwarrior <camille.dorland@gmail.com>
2023-04-28 16:31:28 +0200

    list trainer ok

--- src/Controller/TrainerController.php
+++ src/Controller/TrainerController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Trainer;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class TrainerController extends AbstractController
 {
     #[Route('/trainer', name: 'app_trainer')]
-    public function index(): Response
+    public function index( ManagerRegistry $doctrine): Response
     {
+        $trainers = $doctrine->getRepository( Trainer::class )->findAll();
         return $this->render('trainer/index.html.twig', [
-            'controller_name' => 'TrainerController',
+            'trainers' => $trainers,
         ]);
     }
 }
